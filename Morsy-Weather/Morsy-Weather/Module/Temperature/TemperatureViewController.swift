ab9172e8ee8857f7719da35d58d6aaa9d256391b
Abdelsalam Sawy <abdelsalam.sawy@check24.de>
2023-09-09 13:14:18 +0300

    setup Weather app

---
+++ Morsy-Weather/Morsy-Weather/Module/Temperature/TemperatureViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TemperatureViewController.swift
+//  Morsy-Weather
+//
+//  Created by Abdelsalam Morsy on 09.09.23.
+//
+
+import UIKit
+
+class TemperatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
